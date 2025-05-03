1da5a4a22839be1aa499f3ee4fd89ac19373ec0b
James Baker <jlbaker361@gmail.com>
2025-05-03 15:31:38 -0400

    data

--- cycle_training.py
+++ cycle_training.py
@@ -5,11 +5,15 @@ import argparse
 import time
 import numpy as np
 import os
+from torch.utils.data import DataLoader
+from data_helpers import BrainImageSubjectDataset
 
 parser=argparse.ArgumentParser()
 parser.add_argument("--mixed_precision",type=str,default="no")
 parser.add_argument("--project_name",type=str,default="person")
 parser.add_argument("--gradient_accumulation_steps",type=int,default=4)
+parser.add_argument("--batch_size",type=int,default=4)
+parser.add_argument("--val_split",type=float,default=0.1)
 
 def main(args):
     accelerator=Accelerator(log_with="wandb",mixed_precision=args.mixed_precision,gradient_accumulation_steps=args.gradient_accumulation_steps)
@@ -22,8 +26,14 @@ def main(args):
         "bf16":torch.bfloat16
     }[args.mixed_precision]
     with accelerator.autocast():
-        train_data=[]
-        test_data=[]
+
+        train_fmri=[]
+        train_img=[]
+        train_labels=[]
+
+        test_fmri=[]
+        test_img=[]
+        test_labels=[]
         subject_class_labels={
             1:np.array([1,0,0,0]),
             2:np.array([0,1,0,0]),
@@ -32,6 +42,38 @@ def main(args):
         }
         for sub in [1, 2, 5, 7]:
             path=os.environ["BRAIN_DATA_DIR"]+f"/subj{sub:02d}_fmri_stim_paired.npz"
+            npz_loaded=np.load(path)
+            subject_fmri_train=npz_loaded["fmri_train"]
+            subject_stim_train=npz_loaded["stim_train"]
+            subject_fmri_test=npz_loaded["fmri_test"]
+            subject_stim_test=npz_loaded["fmri_test"]
+
+            n_test=len(subject_fmri_test)
+            n_train=len(subject_fmri_train)
+
+            subject_train_labels=[subject_class_labels[sub] for _ in range(n_train)]
+            subject_test_labels=[subject_class_labels[sub] for _ in range(n_test)]
+
+            train_fmri.extend(subject_fmri_train)
+            train_img.extend(subject_stim_train)
+            train_labels.extend(subject_train_labels)
+
+            test_fmri.extend(subject_fmri_test)
+            test_img.extend(subject_stim_test)
+            test_labels.extend(subject_test_labels)
+
+
+        train_dataset=BrainImageSubjectDataset(train_fmri,train_img,train_labels)
+        test_dataset=BrainImageSubjectDataset(test_fmri,test_img,test_labels)
+
+        train_loader=DataLoader(train_dataset,batch_size=args.batch_size)
+        test_loader=DataLoader(test_dataset,batch_size=args.batch_size,)
+
+
+        
+
+
+
 
 
 
