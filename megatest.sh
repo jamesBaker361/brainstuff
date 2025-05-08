cdb47b53fbae789bec3679d3e474666e9b31eb9b
James Baker <jlbaker361@gmail.com>
2025-05-08 16:33:15 -0400

    megatest

---
+++ megatest.sh
@@ -0,0 +1,7 @@
+sbatch --err=slurm_chip/cycle_test/voxel.err --out=slurm_chip/cycle_test/voxel.out runpygpu_chip.sh cycle_training.py
+sbatch --err=slurm_chip/cycle_test/voxelfp16.err --out=slurm_chip/cycle_test/voxelfp16.out runpygpu_chip.sh cycle_training.py --mixed_precision fp16
+sbatch --err=slurm_chip/cycle_test/voxel_disc.err --out=slurm_chip/cycle_test/voxel_disc.out runpygpu_chip.sh cycle_training.py --use_discriminator
+
+sbatch --err=slurm_chip/cycle_test/array.err --out=slurm_chip/cycle_test/array.out runpygpu_chip.sh cycle_training.py --fmri_type array
+sbatch --err=slurm_chip/cycle_test/arrayfp16.err --out=slurm_chip/cycle_test/arrayfp16.out runpygpu_chip.sh cycle_training.py --mixed_precision fp16 --fmri_type array
+sbatch --err=slurm_chip/cycle_test/array_disc.err --out=slurm_chip/cycle_test/array_disc.out runpygpu_chip.sh cycle_training.py --use_discriminator --fmri_type array
