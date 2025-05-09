f5eec99cd4c2b9c6f71f0e76153bd42fcdaa6cd0
James Baker <jlbaker361@gmail.com>
2025-05-09 11:59:31 -0400

    shorter testing time

--- megatest.sh
+++ megatest.sh
@@ -1,7 +1,7 @@
-sbatch --err=slurm_chip/cycle_test/voxel.err --out=slurm_chip/cycle_test/voxel.out runpygpu_chip.sh cycle_training.py
-sbatch --err=slurm_chip/cycle_test/voxelfp16.err --out=slurm_chip/cycle_test/voxelfp16.out runpygpu_chip.sh cycle_training.py --mixed_precision fp16
-sbatch --err=slurm_chip/cycle_test/voxel_disc.err --out=slurm_chip/cycle_test/voxel_disc.out runpygpu_chip.sh cycle_training.py --use_discriminator
+sbatch --err=slurm_chip/cycle_test/voxel.err --out=slurm_chip/cycle_test/voxel.out runpygpu_chip.sh cycle_training.py --train_limit 4 --test_limit 4 --gradient_accumulation_steps 2 --epochs 2
+sbatch --err=slurm_chip/cycle_test/voxelfp16.err --out=slurm_chip/cycle_test/voxelfp16.out runpygpu_chip.sh cycle_training.py --mixed_precision fp16 --train_limit 4 --test_limit 4 --gradient_accumulation_steps 2 --epochs 2
+sbatch --err=slurm_chip/cycle_test/voxel_disc.err --out=slurm_chip/cycle_test/voxel_disc.out runpygpu_chip.sh cycle_training.py --use_discriminator --train_limit 4 --test_limit 4 --gradient_accumulation_steps 2 --epochs 2
 
-sbatch --err=slurm_chip/cycle_test/array.err --out=slurm_chip/cycle_test/array.out runpygpu_chip.sh cycle_training.py --fmri_type array --kernel_size 4 --n_layers 4 --sublist 1
-sbatch --err=slurm_chip/cycle_test/arrayfp16.err --out=slurm_chip/cycle_test/arrayfp16.out runpygpu_chip.sh cycle_training.py --mixed_precision fp16 --fmri_type array --kernel_size 4 --n_layers 4 --sublist 1
-sbatch --err=slurm_chip/cycle_test/array_disc.err --out=slurm_chip/cycle_test/array_disc.out runpygpu_chip.sh cycle_training.py --use_discriminator --fmri_type array --kernel_size 4 --n_layers 4 --sublist 1
+sbatch --err=slurm_chip/cycle_test/array.err --out=slurm_chip/cycle_test/array.out runpygpu_chip.sh cycle_training.py --fmri_type array --kernel_size 4 --n_layers 4 --sublist 1 --train_limit 4 --test_limit 4 --gradient_accumulation_steps 2 --epochs 2
+sbatch --err=slurm_chip/cycle_test/arrayfp16.err --out=slurm_chip/cycle_test/arrayfp16.out runpygpu_chip.sh cycle_training.py --mixed_precision fp16 --fmri_type array --kernel_size 4 --n_layers 4 --sublist 1 --train_limit 4 --test_limit 4 --gradient_accumulation_steps 2 --epochs 2
+sbatch --err=slurm_chip/cycle_test/array_disc.err --out=slurm_chip/cycle_test/array_disc.out runpygpu_chip.sh cycle_training.py --use_discriminator --fmri_type array --kernel_size 4 --n_layers 4 --sublist 1 --train_limit 4 --test_limit 4 --gradient_accumulation_steps 2 --epochs 2
