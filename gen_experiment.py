0b76772ee7ceea85b397d849de827fe29107a723
James Baker <jlbaker361@gmail.com>
2025-05-10 14:02:14 -0400

    nit

--- gen_experiment.py
+++ gen_experiment.py
@@ -3,7 +3,7 @@ for pairing in ["unpaired","paired"]:
         for recons in ["recons","unrecons"]:
             for disc in ["disc","no_disc"]:
                 name=f"{pairing}_{translate}_{recons}_{disc}"
-                command=f" sbatch --err=slurm_chip/cycle/{name}.err --out=slurm_chip/script_test/{name}.out runpygpu_chip_L40S.sh "
+                command=f" sbatch -J cycle --err=slurm_chip/cycle/{name}.err --out=slurm_chip/script_test/{name}.out runpygpu_chip_L40S.sh "
                 command+=" --epochs 250 --validation_interval 10 --project_name cycle_sub1 --sublist 1 "
                 if pairing=="paired":
                     command+=" --unpaired_image_dataset nouman-10/wikiart_testing "
@@ -17,3 +17,4 @@ for pairing in ["unpaired","paired"]:
                     pass
                 else:
                     print(command)
+        print("")
