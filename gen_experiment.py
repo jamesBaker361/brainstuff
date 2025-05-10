fe30a70888663a006a3f84f94d3d7b2399a43624
James Baker <jlbaker361@gmail.com>
2025-05-10 14:24:29 -0400

    dumb mistakes

--- gen_experiment.py
+++ gen_experiment.py
@@ -3,8 +3,8 @@ for pairing in ["unpaired","paired"]:
         for recons in ["recons","unrecons"]:
             for disc in ["disc","no_disc"]:
                 name=f"{pairing}_{translate}_{recons}_{disc}"
-                command=f" sbatch -J cycle --err=slurm_chip/cycle/{name}.err --out=slurm_chip/script_test/{name}.out runpygpu_chip_L40S.sh "
-                command+=" --epochs 250 --validation_interval 10 --project_name cycle_sub1 --sublist 1 "
+                command=f" sbatch -J cycle --err=slurm_chip/cycle/{name}.err --out=slurm_chip/cycle/{name}.out runpygpu_chip_L40S.sh "
+                command+=" cycle_training.py --epochs 250 --validation_interval 10 --project_name cycle_sub1 --sublist 1 "
                 if pairing=="paired":
                     command+=" --unpaired_image_dataset nouman-10/wikiart_testing "
                 if translate=="trans":
