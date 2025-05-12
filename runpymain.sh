a6ee0d0dbe1ee1342c4ac0d33aa863ba0d54ec63
James Baker <jlbaker361@gmail.com>
2025-05-12 18:01:31 -0400

    new cluster

--- runpymain.sh
+++ runpymain.sh
@@ -34,5 +34,6 @@ export IMAGE_REWARD_PATH="/scratch/jlb638/reward-blob"
 export IMAGE_REWARD_CONFIG="/scratch/jlb638/ImageReward/med_config.json"
 export BRAIN_DATA_DIR='/scratch/jlb638/brain-diffuser/data'
 export SCIKIT_LEARN_DATA="/scratch/jlb638/scikit-learn-data"
+export BRAIN_DATA_DIR="/scratch/jlb638/brain/data"
 srun python3 $@
 conda deactivate
