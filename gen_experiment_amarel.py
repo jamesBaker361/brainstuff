2b0991661993c20368cd97a97e4da31d7669c0b5
James Baker <jlbaker361@gmail.com>
2025-05-13 16:35:32 -0400

    bad gpu

--- gen_experiment_amarel.py
+++ gen_experiment_amarel.py
@@ -3,7 +3,7 @@ for pairing in ["unpaired","paired"]:
         for recons in ["recons","unrecons"]:
             for disc in ["disc","no_disc"]:
                 name=f"{pairing}_{translate}_{recons}_{disc}"
-                command=f" sbatch -J cycle --err=slurm/cycle/{name}.err --out=slurm/cycle/{name}.out --exclude=gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010],gpuk005 runpygpu.sh "
+                command=f" sbatch -J cycle --err=slurm/cycle/{name}.err --out=slurm/cycle/{name}.out --exclude=gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010],gpuk005,gpuk004 runpygpu.sh "
                 command+=" cycle_training.py --epochs 1000 --validation_interval 50 --project_name cycle_sub1 --sublist 1 --fmri_type array "
                 command+=" --gradient_accumulation_steps 16 "
                 if pairing=="paired":
