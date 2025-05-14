ddf4e63d903d512057d1157f344f44292dd39fc5
James Baker <jlbaker361@gmail.com>
2025-05-14 13:44:32 -0400

    fp16

--- gen_experiment_amarel.py
+++ gen_experiment_amarel.py
@@ -5,7 +5,7 @@ for pairing in ["unpaired","paired"]:
                 name=f"{pairing}_{translate}_{recons}_{disc}"
                 command=f" sbatch -J cycle --err=slurm/cycle/{name}.err --out=slurm/cycle/{name}.out --exclude=gpu[005,006,010-014,017,018],cuda[001-008],pascal[001-010],gpuk[001-012] runpygpu.sh "
                 command+=" cycle_training.py --epochs 1000 --validation_interval 50 --project_name cycle_sub1 --sublist 1 --fmri_type array "
-                command+=" --gradient_accumulation_steps 16 "
+                command+=" --gradient_accumulation_steps 16 --mixed_precision fp16 "
                 if pairing=="paired":
                     command+=" --unpaired_image_dataset nouman-10/wikiart_testing "
                 if translate=="trans":
@@ -18,5 +18,5 @@ for pairing in ["unpaired","paired"]:
                     pass
                 else:
                     print(command)
-                    print("sleep 10 ")
+                    print("sleep 1 ")
         print("")
