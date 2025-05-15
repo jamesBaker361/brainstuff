5ee12e9d3a9681357451de071dd4ffb648823bf9
James Baker <jlbaker361@gmail.com>
2025-05-15 00:29:40 -0400

    binding

--- runaccgpu.sh
+++ runaccgpu.sh
@@ -24,6 +24,8 @@
 
 #SBATCH --exclude=gpu[005,006,011],cuda[001-008],pascal[006-010],gpuk[001-012]
 
+#SBATCH --gres-flags=enforce-binding
+
 day=$(date +'%m/%d/%Y %R')
 echo "gpu"  ${day} $SLURM_JOBID "node_list" $SLURM_NODELIST $@  "\n" >> jobs.txt
 module purge
