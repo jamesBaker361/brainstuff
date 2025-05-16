bf4c4f0e6fa7cbad1be992e07ad5efe222377bfd
James Baker <jlbaker361@gmail.com>
2025-05-16 15:23:01 -0400

    exclude

--- gen_super.py
+++ gen_super.py
@@ -1,7 +1,7 @@
 for sub in [1,2,5,7]:
     for res in [0,1,2]:
         name=f"sub_{sub}_res_{res}"
-        command=f"sbatch -J super --out=slurm/super/{name}.out --err=slurm/super/{name}.err runpygpu.sh "
+        command=f"sbatch -J super --out=slurm/super/{name}.out --err=slurm/super/{name}.err --exclude=gpu[005,006,008,010-014,017,018],cuda[001-008],pascal[001-010],gpuk[001-012] runpygpu.sh "
         command+=" super_res_training.py --epochs 250 --gradient_accumulation_steps 8 --project_name super "
         command+=f" --sublist {sub} --residual_blocks {res} "
         print(command)
