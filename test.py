870db80d71765d802810d740b4ef6012ccdd475c
James Baker <jlbaker361@gmail.com>
2025-05-18 10:51:56 -0400

    tokenism

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from datasets import Dataset
 from PIL import Image
+import os
 
 d={
     "label":["image"],
     "image":[Image.open("Cat_November_2010-1a.jpg")]
 
 }
-
-Dataset.from_dict(d).push_to_hub("jlbaker361/testing")
+token=os.environ["HF_TOKEN"]
+Dataset.from_dict(d).push_to_hub("jlbaker361/testing",token=token)
