f98eaf5262b4a7bd5898336ba9a05af236d48f02
James Baker <jlbaker361@gmail.com>
2025-05-19 18:46:23 -0400

    4000

--- gen_super.py
+++ gen_super.py
@@ -1,7 +1,8 @@
-for sub in [1,2,5,7]:
+for sub in [1,2]:
     for res in [3,4]:
-        name=f"sub_{sub}_res_{res}"
-        command=f"sbatch -J super --out=slurm/super/{name}.out --err=slurm/super/{name}.err --exclude=gpu[005,006,008,010-014,017,018],cuda[001-008],pascal[001-010],gpuk[001-012] runpygpu.sh "
-        command+=" super_res_training.py --epochs 750 --gradient_accumulation_steps 8 --project_name super "
-        command+=f" --sublist {sub} --residual_blocks {res} --mixed_precision bf16 "
-        print(command)
+        for epochs in [4000]:
+            name=f"sub_{sub}_{epochs}_res_{res}"
+            command=f"sbatch -J super --out=slurm/super/{name}.out --err=slurm/super/{name}.err runpygpu.sh "
+            command+=f" super_res_training.py --epochs {epochs} --gradient_accumulation_steps 8 --project_name super "
+            command+=f" --sublist {sub} --residual_blocks {res} --mixed_precision bf16 --name {name} "
+            print(command)
