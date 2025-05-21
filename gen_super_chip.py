15b87e628bb755bd942eca54cf5fc638c5fadf59
James Baker <jlbaker361@gmail.com>
2025-05-21 14:25:00 -0400

    big numbers

--- gen_super_chip.py
+++ gen_super_chip.py
@@ -1,6 +1,6 @@
 for sub in [1,2]:
     for res in [3,4]:
-        for epochs in [3000]:
+        for epochs in [4000,5000]:
             name=f"sub_{sub}_{epochs}_res_{res}"
             command=f"sbatch -J super --out=slurm_chip/super/{name}.out --err=slurm_chip/super/{name}.err runpygpu_chip_L40S.sh "
             command+=f" super_res_training.py --epochs {epochs} --gradient_accumulation_steps 8 --project_name super "
