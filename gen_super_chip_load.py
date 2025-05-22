f38a814a1c97843b32e27df3043022ecbf693430
James Baker <jlbaker361@gmail.com>
2025-05-22 11:35:49 -0400

    epochs

--- gen_super_chip_load.py
+++ gen_super_chip_load.py
@@ -1,8 +1,8 @@
 for sub in [1,2]:
     for res in [3,4]:
-        for epochs in [4000,5000]:
+        for epochs in [2000,3000]:
             name=f"sub_{sub}_{epochs}_res_{res}"
-            command=f"sbatch -J super --out=slurm_chip/super_load/{name}.out --err=slurm_chip/super_load/{name}.err runpygpu_chip.sh "
+            command=f"sbatch -J load --out=slurm_chip/super_load/{name}.out --err=slurm_chip/super_load/{name}.err runpygpu_chip.sh "
             command+=f" super_res_training.py --epochs {epochs} --gradient_accumulation_steps 8 --project_name super "
             command+=f" --sublist {sub} --residual_blocks {res} --mixed_precision bf16 --name {name} --load "
             print(command)
